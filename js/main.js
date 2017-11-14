8608af9bf96caff5bc2d4967e0c5de10fe06cf65
soni17 <elb004@gmail.com>
2017-11-14 16:42:35 -0500

    add functionality to switch

--- js/main.js
+++ js/main.js
@@ -1,2 +1,21 @@
+document.addEventListener('DOMContentLoaded',function () {
 
+  const status = document.querySelector(".status");
+  const switchButton = document.querySelector("button");
+  const body = document.querySelector("body");
 
+  switchButton.addEventListener('click',function () {
+
+    if (this.className === "switch on"){
+      body.className = "dark";
+      switchButton.className = "switch off";
+      status.innerText = "Hey, who turned off the lights?";
+    }else if (this.className === "switch off"){
+      body.className = "light";
+      switchButton.className = "switch on";
+      status.innerText = "It's so bright in here!";
+    }
+
+  });
+
+});
